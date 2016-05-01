41b86673e72726eda6d9e3480996fbd9e9c2cd12
bansicloud2 <chet@zendodo.com>
2016-05-01 08:52:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 })
 
-// for facebook verification
+// for facebook verificatione
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
